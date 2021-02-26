78e696670874f50a49387c64c2a897a2e9ee3a56
alexmac <amatsibarov@gmail.com>
2021-02-26 12:09:57 +0300

    fix eslint warns

--- index.js
+++ index.js
@@ -1,151 +1,151 @@
-const fs = require('fs');
-const path = require('path');
-const events = require('events');
+const fs = require('fs')
+const path = require('path')
+const events = require('events')
 
 class NReadline extends events.EventEmitter {
   constructor({ filepath, encoding = 'utf8', start, limit, skipEmptyLines }) {
-    super();
-    this._readStream = null;
-    this._filepath = path.normalize(filepath);
-    this._streamOptions = { encoding };
-    this._skipEmptyLines = !!skipEmptyLines;
+    super()
+    this._readStream = null
+    this._filepath = path.normalize(filepath)
+    this._streamOptions = { encoding }
+    this._skipEmptyLines = !!skipEmptyLines
 
-    this._from = start || 0;
+    this._from = start || 0
     if (limit) {
-      this._to = this._from + limit;
+      this._to = this._from + limit
     }
-    this._lines = [];
-    this._lineFragment = '';
-    this._lineNumber = 0;
-    this._isPaused = false;
-    this._isStopping = false;
-    this._isStopped = false;
+    this._lines = []
+    this._lineFragment = ''
+    this._lineNumber = 0
+    this._isPaused = false
+    this._isStopping = false
+    this._isStopped = false
   }
 
   start() {
-    setImmediate(() => this._initStream());
+    setImmediate(() => this._initStream())
   }
 
   pause() {
-    this._isPaused = true;
+    this._isPaused = true
   }
 
   resume() {
-    this._isPaused = false;
-    setImmediate(() => this._nextLine());
+    this._isPaused = false
+    setImmediate(() => this._nextLine())
   }
 
   stop() {
-    this._readStream.destroy();
-    this._isStopping = true;
-    this._lines = [];
-    setImmediate(() => this._stop());
+    this._readStream.destroy()
+    this._isStopping = true
+    this._lines = []
+    setImmediate(() => this._stop())
   }
 
   _initStream() {
-    this._readStream = fs.createReadStream(this._filepath, this._streamOptions);
+    this._readStream = fs.createReadStream(this._filepath, this._streamOptions)
 
     this._readStream.on('error', err => {
-      this.emit('error', err);
-    });
+      this.emit('error', err)
+    })
 
     this._readStream.on('open', () => {
-      this.emit('open');
-    });
+      this.emit('open')
+    })
 
     this._readStream.on('data', (data) => {
-      this._readStream.pause();
-      var dataAsString = data;
-      this._lines = this._lines.concat(dataAsString.split(/(?:\n|\r\n|\r)/g));
+      this._readStream.pause()
+      var dataAsString = data
+      this._lines = this._lines.concat(dataAsString.split(/(?:\n|\r\n|\r)/g))
 
-      this._lines[0] = this._lineFragment + this._lines[0];
-      this._lineFragment = this._lines.pop() || '';
+      this._lines[0] = this._lineFragment + this._lines[0]
+      this._lineFragment = this._lines.pop() || ''
 
-      setImmediate(() => this._nextLine());
-    });
+      setImmediate(() => this._nextLine())
+    })
 
     this._readStream.on('end', () => {
-      this._isStopping = true;
-      setImmediate(() => this._nextLine());
-    });
+      this._isStopping = true
+      setImmediate(() => this._nextLine())
+    })
   }
 
   _nextLine() {
     if (this._isPausedOrFinished()) {
-      return;
+      return
     }
     if (this._handleFrom()) {
-      return;
+      return
     }
     if (this._handleTo()) {
-      return;
+      return
     }
     if (this._handleEmptyLinesBuffer()) {
-      return;
+      return
     }
 
-    let line = this._lines.shift();
+    let line = this._lines.shift()
     if (!this._skipEmptyLines || line.length > 0) {
-      this.emit('line', line, this._lineNumber);
+      this.emit('line', line, this._lineNumber)
     }
 
-    this._lineNumber++;
-    setImmediate(() => this._nextLine());
+    this._lineNumber++
+    setImmediate(() => this._nextLine())
   }
 
   _stop() {
     if (!this._isStopped) {
-      this._isStopped = true;
-      this.emit('end');
+      this._isStopped = true
+      this.emit('end')
     }
   }
 
   _isPausedOrFinished() {
-    return this._isPaused || this._isStopped;
+    return this._isPaused || this._isStopped
   }
 
   _handleFrom() {
     if (!this._from || this._lineNumber >= this._from) {
-      return;
+      return
     }
     if (this._lines.length === 0) {
       if (this._isStopping) {
-        this._stop();
+        this._stop()
       } else {
-        this._readStream.resume();
+        this._readStream.resume()
       }
     } else {
-      this._lines.shift();
-      this._lineNumber++;
-      setImmediate(() => this._nextLine());
+      this._lines.shift()
+      this._lineNumber++
+      setImmediate(() => this._nextLine())
     }
-    return true;
+    return true
   }
 
   _handleTo() {
     if (!this._to || this._lineNumber < this._to) {
-      return;
+      return
     }
-    this._isStopping = true;
-    this._stop();
-    return true;
+    this._isStopping = true
+    this._stop()
+    return true
   }
 
   _handleEmptyLinesBuffer() {
     if (this._lines.length !== 0) {
-      return;
+      return
     }
     if (this._isStopping) {
       if (this._lineFragment) {
-        this.emit('line', this._lineFragment, this._lineNumber);
-        this._lineFragment = '';
+        this.emit('line', this._lineFragment, this._lineNumber)
+        this._lineFragment = ''
       }
-      this._stop();
+      this._stop()
     } else {
-      this._readStream.resume();
+      this._readStream.resume()
     }
-    return true;
+    return true
   }
 }
 
-module.exports = NReadline;
+module.exports = NReadline
